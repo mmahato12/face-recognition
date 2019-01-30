6ec98296773f6e0ff7a01c47830053911076604c
mmahato12 <mkm121296@gmail.com>
2019-01-30 11:56:59 +0530

    Face Recognition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
@@ -9,9 +11,8 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        
-        {/*<ImageLinkForm />
-        */}
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
