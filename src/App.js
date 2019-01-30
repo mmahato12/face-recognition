b05b7ac8e1ef07dfa47010397a6bf842781d2a89
mmahato12 <mkm121296@gmail.com>
2019-01-30 19:56:53 +0530

    Face Detection

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from '../node_modules/react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+        params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
